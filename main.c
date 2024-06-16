055dab9931c7cf78fe4c5a6d7091e23465d73a64
Tim940328 <tim0968777890@gmail.com>
2024-06-17 03:01:43 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,24 +1,17 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#define  MAX_STUDENT 10
+#define  MAX_STUDENT 100
 #include "myhead.h"
+
 int main()
 {
-    Student students[MAX_STUDENT];
-    randominitStudents(students, MAX_STUDENT);
+    SRecord srecords[MAX_STUDENT];
+    int numSRecords = readSRecordsFromCSV("records.csv", srecords, MAX_STUDENT);
 
-    sortStudents(students, MAX_STUDENT, "name");
-    printStudentsByname(students, MAX_STUDENT);
-   
-    sortStudents(students, MAX_STUDENT, "department");
-    printStudentsBydepartment(students, MAX_STUDENT);
+    printSRecords(srecords, numSRecords);
 
-    sortStudents(students, MAX_STUDENT, "grade");
-    printStudentsBygrade(students, MAX_STUDENT);
-
-    sortStudents(students, MAX_STUDENT, "SID");
-    printStudentsBySID(students, MAX_STUDENT);
-
+    sortSRcord(srecords, numSRecords, "total");
+    printf("\n\nAfter sorting by total:\n");
+    printSRecords(srecords, numSRecords);
+    
     return 0;
 }
